557c33252fe9297cf8ef01381831c1691fd5924b
Jeffrey Beyer <jb7656@ship.edu>
2017-12-04 10:19:08 -0500

    Added comments for state classes

--- src/states/FaintedState.java
+++ src/states/FaintedState.java
@@ -2,6 +2,10 @@ package states;
 
 import lifeform.*;
 
+/**
+ * State class for a pokemon that has died (fainted)
+ * @author Michael Foreman
+ */
 public class FaintedState extends HealthState{
 
 	public FaintedState(Pokemon poke)
@@ -9,11 +13,19 @@ public class FaintedState extends HealthState{
 		this.pokemon = poke;
 	}
 	
+	/**
+	 * Ensures a pokemon will do no damage if a player tries
+	 * to attack when a poemon is fainted
+	 */
 	@Override
 	public double getAttackFraction() {
 		return NO_DAMAGE;
 	}
 	
+	/**
+	 * Method to transition to the correct state if a pokemon
+	 * is healed
+	 */
 	@Override
 	public void checkState(int currentHealth, int maxHealth) {
 		if((currentHealth < maxHealth) && (currentHealth > 0))
